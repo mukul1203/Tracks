f0f4fbeca69bfa98252de8053a50a3484aa8be1d
Mukul Agrawal <mukuluec@gmail.com>
2023-09-02 18:18:08 +0000

    Trying to get the current location marked. Not working.

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView,{Marker} from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState({latitude:10, longitude:10});
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect( () => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({latitude:location.coords.latitude, longitude:location.coords.longitude});
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map}>
         <Marker
-        coordinate={{latitude:10, longitude:10}}
+        coordinate={location}
         title="Current location"
         description="Current location"
         />

21e8cbba422280cdc164b8aea7a1cbe7ec7ea62c
Mukul Agrawal <mukuluec@gmail.com>
2023-09-03 07:33:30 +0000

    Region as state. Initial region focusses on current location

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import MapView,{Marker} from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import * as Location from 'expo-location';
@@ -6,28 +6,43 @@ import * as Location from 'expo-location';
 export default function App() {
   const [location, setLocation] = useState({latitude:10, longitude:10});
   const [errorMsg, setErrorMsg] = useState(null);
-  useEffect( () => {
+  const [region, setRegion] = useState(
+                                      {
+                                        latitude: 10,
+                                        longitude: 10,
+                                        latitudeDelta: 0.0922,
+                                        longitudeDelta: 0.0421
+                                      }
+                                      );
+  useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
         return;
       }
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation({latitude:location.coords.latitude, longitude:location.coords.longitude});
+      let {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({});
+      setLocation({latitude, longitude});
+      setRegion({...region, latitude, longitude});
     })();
   }, []);
+
+  const onRegionChange = useCallback((inRegion, gesture)=>{
+    if(!gesture.isGesture) //FIXME: this won't work for apple maps.
+      setRegion(inRegion);
+  });
 
   return (
     <View style={styles.container}>
       {
       errorMsg ? 
       <Text>errorMsg</Text> : 
-      <MapView style={styles.map}>
+      <MapView style={styles.map} region={region}
+      onRegionChange={onRegionChange}>
         <Marker
         coordinate={location}
         title="Current location"
-        description="Current location"
+        description={JSON.stringify(location)}
         />
       </MapView>
       }

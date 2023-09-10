a4190b03a2087bfcc515f5d83047eecc3e301545
Mukul Agrawal <mukuluec@gmail.com>
2023-09-10 06:51:10 +0000

    Persist authentication across sessions

--- AwesomeProject/config/firebaseConfig.js
+++ AwesomeProject/config/firebaseConfig.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from "firebase/app";
 import 'firebase/auth';
 import Constants from 'expo-constants';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
 // const analytics = getAnalytics(app);

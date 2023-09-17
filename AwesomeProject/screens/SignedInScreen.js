58185356b8f5cbab8a83bdfa4570c2bef53f8eda
mukuluec <mukuluec@gmail.com>
2023-09-17 18:06:52 +0530

    delete group functionality TODO: allow delete by the creator of group

--- AwesomeProject/screens/SignedInScreen.js
+++ AwesomeProject/screens/SignedInScreen.js
@@ -1,25 +1,26 @@
 import { getAuth } from "firebase/auth";
 import { Button, FlatList, StyleSheet, View, Text } from "react-native";
 import { useInvites } from "../utils/hooks/useInvites";
-import { joinGroup } from "../utils/hooks/useGroup";
+import { deleteGroup, joinGroup } from "../utils/hooks/useGroup";
 import { userSignOut } from "../utils/hooks/useAuthentication";
 
 const auth = getAuth();
 export default function SignedInScreen({ navigation }) {
   const user = auth.currentUser;
-  const [invites] = useInvites([]);
-  const JoinGroupListItem = (invite)=>(
+  const [groupIds] = useInvites([]);
+  const JoinGroupListItem = (groupId)=>(
     <View style={{...styles.container, flexDirection:"row"}}>
-    <Text>{invite}</Text>
-    <Button title="Join" style={styles.button} onPress={()=>joinGroup(invite)}/>
+    <Text>{groupId}</Text>
+    <Button title="Join" style={styles.button} onPress={()=>joinGroup(groupId)}/>
+    <Button title="Delete" style={styles.button} onPress={()=>deleteGroup(groupId)}/>
     </View>
   );
   return (
     <View style={styles.container}>
       <Text>Welcome {user?.email}!</Text>
       <FlatList 
-        keyExtractor={(invite)=>invite}
-        data={invites}
+        keyExtractor={(groupId)=>groupId}
+        data={groupIds}
         renderItem={({item}) => JoinGroupListItem(item)}
       >
       </FlatList>
--- AwesomeProject/utils/hooks/useGroup.js
+++ AwesomeProject/utils/hooks/useGroup.js
@@ -19,20 +19,31 @@ export const createGroup = async (emails) => {
     await set(ref(database, 'users/'+auth.currentUser.uid+'/groupId'), groupRef.key);
     //Also update invitees' invites list with this group id. Invite self also.
     await invite([...emails, auth.currentUser.email], groupRef.key);
-};
+}
 
 export const exitGroup = (groupId) => {
     const groupIdRef = ref(database, 'users/'+auth.currentUser.uid+'/groupId');
     set(groupIdRef, null);
     //TODO: also update the groups/groupId
-};
+}
 
 export const joinGroup = (groupId) => {
     //set the user's groupId to this groupId
     //no need to await
     const groupIdRef = ref(database, 'users/'+auth.currentUser.uid+'/groupId');
     set(groupIdRef, groupId);
-  };
+}
+
+export const deleteGroup = (groupId) => {
+    //remove the group from user's invite list
+    //remove the group from groups list
+    //TODO: remove the group entry from every invite list of any user (how to?)
+    //TODO: optimize by a single update call
+    const inviteEntryRef = ref(database, 'users/'+auth.currentUser.uid+'/invites/'+groupId);
+    set(inviteEntryRef, null);
+    const groupEntryRef = ref(database, 'groups/'+groupId);
+    set(groupEntryRef, null);
+}
 
 export function useGroup(init) {
     const [groupId, setGroupId] = useState(init);
